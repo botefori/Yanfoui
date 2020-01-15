f7237af5bdb82c9aa7934e33f7982993e7dee68d
Ibrahima <icamara@wynd.eu>
2020-01-15 09:40:27 +0100

    implement crud for customer entity

--- app/Controllers/CustomerController.cs
+++ app/Controllers/CustomerController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using app.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace app.Controllers
 {
@@ -16,6 +18,24 @@ namespace app.Controllers
         public CustomerController(app.Data.AppContext context)
         {
             _context = context;
+        }
+
+        // GET api/customers/5
+        /// <summary>
+        /// Gets customer with specified id.
+        /// </summary>
+        /// <param name="id">The id of the customer.</param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Customer>> GetCustomerItem(int id)
+        {
+            var customer = await _context.Customers.FindAsync(id);
+
+            if(customer == null)
+            {
+                return NotFound();
+            }
+            return customer;
         }
         
 
@@ -32,7 +52,57 @@ namespace app.Controllers
             return customersItem.ToArray();
         }
 
-        
+         // POST api/values
+        /// <summary>
+        /// Creates new customer.
+        /// </summary>
+        /// <param name="customer">The id of the value.</param>
+        [HttpPost]
+        public async Task<ActionResult<Customer>> Post(Customer customer)
+        {
+            this._context.Customers.Add(customer);
+            await this._context.SaveChangesAsync(); 
+
+            return CreatedAtAction(nameof(GetCustomerItem), new { id = customer.ID}, customer);
+        }
+
+
+         // PUT: api/customers/5
+         [HttpPut("{id}")]
+        public async Task<IActionResult> PutCustomer(long id, Customer customer)
+        {
+           if(id != customer.ID)
+           {
+               return BadRequest();
+           }
+
+           _context.Entry(customer).State = EntityState.Modified;
+
+           try
+           {
+               await _context.SaveChangesAsync();
+           }catch(DbUpdateConcurrencyException)
+           {
+            
+           }
+
+           return NoContent();
+        }
+
+        // DELETE: api/customers/4
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Customer>> DeleteCustomer(int id)
+        {
+            var customer = await _context.Customers.FindAsync(id);
+            if(customer == null)
+            {
+                return NotFound();
+            }
+            _context.Customers.Remove(customer);
+            await _context.SaveChangesAsync();
+
+            return customer;
+        }
 
     }
 }
